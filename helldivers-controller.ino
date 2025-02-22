4b2454100d269255cd3c5244c7b5d6856e55f142
dperezmavro <dio.pm@live.com>
2025-02-22 18:54:25 +0000

    wired to support toggle switches for secondary function

--- helldivers-controller.ino
+++ helldivers-controller.ino
@@ -2,32 +2,50 @@
 #include <Mouse.h>
 #include <ezButton.h>
 
-#define REINFORCE_PIN 2
-#define RESUPPLY_PIN 3
-#define STALWART_PIN 4
-#define SENTRY_PIN 5
+#define PIN_SLOT_1_BTN 2
+#define PIN_SLOT_1_TOGGLE 10
+#define PIN_SLOT_2_BTN 3
+#define PIN_SLOT_2_TOGGLE 10  // 14
+#define PIN_SLOT_3_BTN 4
+#define PIN_SLOT_3_TOGGLE 10  // 15
+#define PIN_SLOT_4_BTN 5
+#define PIN_SLOT_4_TOGGLE 10  // 16
+
 
 #define WAIT_BETWEEN_KEY_PRESS 150
 #define DEBOUNCE_TIME 50
 
+// button 1
 const char reinforceCode[] = "wsdaw";
 const char resupplyCode[] = "sswd";
+
+// button 2
 const char stalwartCode[] = "saswwa";
+const char machineGunCode[] = "saswd";
+
+// button 3
+const char guardDogRoverCode[] = "saswass";
 const char sentryAutocannonCode[] = "swdwaw";
 
-ezButton reinforceBtn(REINFORCE_PIN);
-ezButton resupplyBtn(RESUPPLY_PIN);
-ezButton stalwartBtn(STALWART_PIN);
-ezButton sentryBtn(SENTRY_PIN);
+// button 4
+const char eagleNapalmAirstrikeCode[] = "wdsw";
+const char fiveHundredKgAirstrikeCode[] = "wdsss";
+
+ezButton slot_1(PIN_SLOT_1_BTN);
+ezButton slot_2(PIN_SLOT_2_BTN);
+ezButton slot_3(PIN_SLOT_3_BTN);
+ezButton slot_4(PIN_SLOT_4_BTN);
 
 void setup() {
   Serial.begin(9600);
 
+  pinMode(15, INPUT);
+
   // setup buttons
-  resupplyBtn.setDebounceTime(DEBOUNCE_TIME);
-  stalwartBtn.setDebounceTime(DEBOUNCE_TIME);
-  reinforceBtn.setDebounceTime(DEBOUNCE_TIME);
-  sentryBtn.setDebounceTime(DEBOUNCE_TIME);
+  slot_2.setDebounceTime(DEBOUNCE_TIME);
+  slot_3.setDebounceTime(DEBOUNCE_TIME);
+  slot_1.setDebounceTime(DEBOUNCE_TIME);
+  slot_4.setDebounceTime(DEBOUNCE_TIME);
 
   // start kbd/mouse libraries
   Keyboard.begin();
@@ -35,29 +53,35 @@ void setup() {
 }
 
 void loop() {
-  resupplyBtn.loop();
-  reinforceBtn.loop();
-  stalwartBtn.loop();
-  sentryBtn.loop();
+  slot_2.loop();
+  slot_1.loop();
+  slot_3.loop();
+  slot_4.loop();
 
-
-  if (reinforceBtn.isPressed()) {
-    callStratagem(reinforceCode);
-  } else if (stalwartBtn.isPressed()) {
-    callStratagem(stalwartCode);
-  } else if (resupplyBtn.isPressed()) {
-    callStratagem(resupplyCode);
-  } else if (sentryBtn.isPressed()) {
-    callStratagem(sentryAutocannonCode);
+  if (slot_1.isPressed()) {
+    callStratagem(reinforceCode, stalwartCode, PIN_SLOT_1_TOGGLE);
+  } else if (slot_2.isPressed()) {
+    callStratagem(resupplyCode, guardDogRoverCode, PIN_SLOT_2_TOGGLE);
+  } else if (slot_3.isPressed()) {
+    callStratagem(sentryAutocannonCode, sentryAutocannonCode, PIN_SLOT_3_TOGGLE);
+  } else if (slot_4.isPressed()) {
+    callStratagem(fiveHundredKgAirstrikeCode, eagleNapalmAirstrikeCode, PIN_SLOT_4_TOGGLE);
   }
 }
 
-void callStratagem(char s[]) {
+void callStratagem(char primaryStratagem[], char secondaryStratagem[], int mode_pin) {
+  int p = digitalRead(mode_pin);
+  char* s = p > 0 ? primaryStratagem : secondaryStratagem;
+  Serial.print(p);
+  Serial.print(" ");
+  Serial.println(s);
+  // return;
+
   Keyboard.press(KEY_LEFT_CTRL);
   delay(WAIT_BETWEEN_KEY_PRESS);
 
-  for (int i = 0; i < strlen(s); i++) {
-    char c = s[i];
+  for (int i = 0; i < strlen(primaryStratagem); i++) {
+    char c = primaryStratagem[i];
 
     Keyboard.press(c);
     delay(WAIT_BETWEEN_KEY_PRESS);
